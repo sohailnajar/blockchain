6f3f3748508ebe744eae12156e3aa18db21d1f49
sohailnajar <contact.sohail@gmail.com>
2017-12-09 20:13:14 +0800

    adds flask server

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,40 @@
-class Blockchsi(object):
+import hashlib
+import json
+from time import time
+from textwrap import dedent
+from uuid import uuid4
+
+from flask import Flask
+
+
+class Blockchain(object):
     def __init__(self):
-        self.chin = []
-        self.current_transaction = []
+        self.chain = []
+        self.current_transactions = []
+        
+        # creates a genesis block
+        self.new_block(previous_hash=1, proof=100)
         
         
-    def new_block(self):
-        # creates a new block and adds it to the chain
-        pass
+    def new_block(self, proof, previous_hash=None):
+        # creates a new block in the Blockchain
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        
+        # reset the current list of transactions
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
     
     def new_transaction(self, sender, recipient, amount):
         """
         Creates a new tansaction to go into next mined block
-        
-    
         """
         self.current_transaction.append({
             'sender': sender,
@@ -20,16 +42,73 @@ class Blockchsi(object):
             'amount': amount,
             })
         return self.last_block['index'] + 1
-    
-    @staticmethod
-    def hash(block):
-        # hash a block
-        pass
-    
-    
+
     @property
     def last_block(self):
         # return the last block in the chain
-        pass
+        return self.chain[-1]
     
     
+    @staticmethod
+    def hash(block):
+        """ 
+        creates a SHA-256 hash of a block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        
+        # We must make sure that the Dictionary is ordered or we will have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+        
+        
+        
+    def proof_of_work(self, last_proof):
+        """
+        Simpe proof of work algorith:
+        - Find a number p such that hash(pp') contains leading 4 zeroes
+        - p is the previous proof, and p' is the new proof
+        
+        :param last_proof: <int>
+        :return: <int>
+        """
+        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+            
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validtes proof of work
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+    
+    
+
+# Instantiate our node
+app = Flask(__name__)
+
+# generate a globally unique address for this
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+    'chain': blockchain.chain,
+    'length': len(blockchain.chain),
+    }
+    
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
