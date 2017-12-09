5ee96b6eb3f2be28f94727613fdc182aa7c77261
sohailnajar <contact.sohail@gmail.com>
2017-12-09 19:24:30 +0800

    updates new transactions method

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,35 @@
-ss
+class Blockchsi(object):
+    def __init__(self):
+        self.chin = []
+        self.current_transaction = []
+        
+        
+    def new_block(self):
+        # creates a new block and adds it to the chain
+        pass
+    
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new tansaction to go into next mined block
+        
+    
+        """
+        self.current_transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+            })
+        return self.last_block['index'] + 1
+    
+    @staticmethod
+    def hash(block):
+        # hash a block
+        pass
+    
+    
+    @property
+    def last_block(self):
+        # return the last block in the chain
+        pass
+    
+    
